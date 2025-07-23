e0863c353758d563df1491e26343b42fec02af0b
Christophe Godet <godet.chris59@gmail.com>
2025-07-23 16:03:49 +0200

    feat: setup basic app structure with RootLayout, Header, and Home page
    - Add RootLayout with global Barlow font, metadata, header, and <main> for main content
    - Create Header with clickable logo linking to home and contact page link
    - Build Home page with welcome message and CTA button to bibliothèque
    - Work on responsive design for better mobile adaptation

---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <Link href="/">
+          <img src="/Arkadia.png" alt="Logo" />
+        </Link>
+        <Link href="/Contact">contact me</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
