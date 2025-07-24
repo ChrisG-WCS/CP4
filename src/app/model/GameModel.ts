83aad2e50089f9315acf555c0f791e0ca1d3c0aa
Christophe Godet <godet.chris59@gmail.com>
2025-07-24 21:13:53 +0200

    feat: Complete DB Connection and API Routes Implementation
    - Configured and established connection to the MySQL database.
    - Implemented `GET /api/games` API route for retrieving the list of games.
    - Implemented `POST /api/games` API route to allow adding new games.
    - Connected frontend (Bibliotheque page) with backend for displaying (GET) and creating (POST) data.
    - Resolved environment variable configuration issues and API file path errors.
    - Correctly displaying game images via URLs stored in the database.

---
+++ src/app/model/GameModel.ts
@@ -0,0 +1,5 @@
+export interface GameModel {
+  img: string;
+  title: string;
+  description: string;
+}
--- src/app/model/game.ts
+++
@@ -1,9 +0,0 @@
-export interface GameModel {
-  img: string;
-  title: string;
-  alt: string;
-  descrition: string;
-  plateforme: string;
-  genre: string;
-  systeme: string;
-}
